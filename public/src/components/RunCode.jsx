d34e4fb006281c231a4edd65289d5eda20a80ee2
Tom Scanlan <tscanlan@stratadecision.com>
2019-05-06 11:22:57 -0500

    stupid unicode

--- public/src/components/RunCode.jsx
+++ public/src/components/RunCode.jsx
@@ -26,6 +26,9 @@ class RunCode extends React.Component{
         });
     }
     onChange = (x,y) =>{
+        var x = x
+  .replace(/[\u2018\u2019]/g, "'")
+  .replace(/[\u201C\u201D]/g, '"');
         this.setState({code:x});
     }
     flipView = () =>{
