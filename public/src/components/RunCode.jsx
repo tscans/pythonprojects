952601a2647dd987e30a1b941ba0c83c6fdafcee
Tom Scanlan <tscanlan@stratadecision.com>
2019-05-06 11:26:37 -0500

    patch

--- public/src/components/RunCode.jsx
+++ public/src/components/RunCode.jsx
@@ -26,9 +26,10 @@ class RunCode extends React.Component{
         });
     }
     onChange = (x,y) =>{
-        var x = x
-  .replace(/[\u2018\u2019]/g, "'")
-  .replace(/[\u201C\u201D]/g, '"');
+        x = x.replace(/[“]/g,'"');
+        x = x.replace(/[”]/g,'"');
+        x = x.replace(/[‘]/g,"'");
+        x = x.replace(/[’]/g,"'");
         this.setState({code:x});
     }
     flipView = () =>{
